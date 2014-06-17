717a0f272d5d5175600bbf8f9eb5b8de2dc20c6f
sunheng <sunhengtaing@gmail.com>
2014-06-16 21:30:26 -0400

    Added total online and chatroom online

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 	var url = window.location.href;
-	var chatroom_name = url.substring(url.indexOf('chatroom') + 9);
+	var chatroom = url.substring(url.indexOf('chatroom') + 9);
 	var username;
 
 	var socket = io.connect('/socket');
 
-	$('#title').text("Welcome to " + chatroom_name);
+	$('#title').text("Welcome to " + chatroom);
 	$('.modal').modal('show');
 
 	$('#register').submit(function(){
 		username = $('#username').val();
-		socket.emit('join', { 
-			username : username,
-			chatroom : chatroom_name});
+		socket.emit('join', {
+			"username" : username,
+			"chatroom" : chatroom});
 		$('.modal').modal('hide');
 		return false;
 	});
@@ -29,5 +29,11 @@ $(document).ready(function() {
 		$('#chatWindow').append('<p><b>' + data.username + ':</b> '+ data.message + '</p>');
 		var objDiv = document.getElementById("chatWindow");
 		objDiv.scrollTop = objDiv.scrollHeight;
-	});	
+	});
+
+	socket.on('chatroomVisitors', function(total) {
+		$('#chatroomVisitors').text("Total people in this chatroom: " + total);
+		var objDiv = document.getElementById("chatWindow");
+		objDiv.scrollTop = objDiv.scrollHeight;
+	});
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
-	$(document).ready(function() {
-		$('#create').submit(function(event) {
-			var url = window.location.href;
-			var direct_url = url + 'chatroom/' + $('#createField').val();
-			$(location).attr('href', direct_url);
-			return false;
-		});	
-		$('#join').submit(function(event) {
-			var url = window.location.href;
-			var direct_url = url + 'chatroom/' + $('#joinField').val();
-			$(location).attr('href', direct_url);
-			return false;
-		});
+	var socket = io.connect('/socket');
+	socket.emit('connected');
+
+	$('#create').submit(function(event) {
+		var url = window.location.href;
+		var directURL = url + 'chatroom/' + $('#createField').val();
+		$(location).attr('href', directURL);
+		return false;
 	});
+
+	$('#join').submit(function(event) {
+		var url = window.location.href;
+		var directURL = url + 'chatroom/' + $('#joinField').val();
+		$(location).attr('href', directURL);
+		return false;
+	});
+
+	socket.on('totalVisitors', function(total) {
+		$('#totalVisitors').text('Total Online: ' + total);
+	});
+
 });
